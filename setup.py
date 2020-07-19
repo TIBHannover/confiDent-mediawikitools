8e061f306568ef017db9d65269290640b8e2dff3
Castro0o <andre@andrecastro>
2020-07-19 15:39:51 +0200

    setup + manifest

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup
+from pathlib import Path
+
+README_path = Path.cwd() / 'README.md'
+with open(README_path, 'r') as README_f:
+    README = README_f.read()
+
 setup(
     name='mediawikitools',
     version='0.1.0',
@@ -9,6 +15,11 @@ setup(
     author_email='andre.castro@tib.eu',
     description='A series of Python mwclient library wrappers, to perform '
                 'actions in Mediawiki instances',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    classifiers=['Programming Language :: Python',
+        'Programming Language :: Python :: 3.7' ],
+    keywords='mediawiki wikipedia',
     install_requires=['mwclient'],
 )
 
